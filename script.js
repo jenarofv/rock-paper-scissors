4b878c48da8e4683696d1b3233bee226de7ba6c1
jenaro fv <jen4r0@gmail.com>
2025-09-21 17:09:42 -0600

    working functions to get player choices and play one round

---
+++ script.js
@@ -0,0 +1,46 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  computerChoiceNum = Math.floor(Math.random() * 3);
+  return choices[computerChoiceNum];
+}
+
+function getHumanChoice() {
+  const humanChoice = prompt("select one:\n- rock\n- paper\n- scissors");
+  return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Tie, you both chose " + humanChoice);
+    return;
+  }
+  const verbs = {
+    // how does each one win?
+    scissors: "cut",
+    paper: "covers",
+    rock: "breaks"
+  }
+  let humanWins;
+  switch (humanChoice) {
+    case "rock":
+      humanWins = computerChoice === "scissors";
+      break;
+    case "paper":
+      humanWins = computerChoice === "rock";
+      break;
+    case "scissors":
+      humanWins = computerChoice === "paper";
+      break;
+  }
+  if (humanWins) {
+    console.log(`You won! ${humanChoice} ${verbs[humanChoice]} ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`Computer wins, ${computerChoice} ${verbs[computerChoice]} ${humanChoice}`);
+    computerScore++;
+  }
+}
